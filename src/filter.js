b51ebc10bf26c0475462713f402047a9d27816b7
GManTheMarioGuy <155935644+GManTheMarioGuy@users.noreply.github.com>
2025-04-08 22:46:51 -0500

    slightly more robust

--- src/filter.js
+++ src/filter.js
@@ -38,9 +38,59 @@ function add(word, severity = 'PROFANE') {
 function filterText(text) {
   let filteredText = text;
 
+  const substitutions = {
+    'a': /[4@]/g,
+    'b': /[8]/g,
+    'c': /[<]/g,
+    'e': /[3]/g,
+    'f': /[ph]/g,
+    'g': /[9]/g,
+    'i': /[1!|]/g,
+    'l': /[|1]/g,
+    'o': /[0()]/g,
+    's': /[5$]/g,
+    't': /[7+]/g,
+    'z': /[2]/g,
+    '0': /[oO]/g,
+    '1': /[il|!]/g,
+    '2': /[z]/g,
+    '3': /[e]/g,
+    '4': /[a@]/g,
+    '5': /[s$]/g,
+    '7': /[t]/g,
+    '!': /[i|l|1]/g,
+    '@': /[a]/g,
+    '$': /[s]/g,
+    '|': /[l]/g,
+    '(': /[o]/g,
+    ')': /[o]/g,
+    '+': /[t]/g,
+    '=': /[e]/g,
+    '[': /[l]/g,
+    ']': /[t]/g,
+    '{': /[c]/g,
+    '}': /[d]/g,
+    '\\': /[m]/g,
+    ';': /[j]/g,
+    ':': /[i]/g,
+    '"': /[a]/g,
+    "'": /[l]/g,
+    ',': /[c]/g,
+    '.': /[o]/g,
+    '/': /[r]/g,
+    '?': /[c]/g
+  };
+
   profanityList.forEach(({ word }) => {
     const escapedWord = word.replace(/[.*+?^=!:${}()|\[\]\/\\]/g, "\\$&");
-    const regex = new RegExp(`${escapedWord}\\w*`, 'gi');
+
+    let pattern = escapedWord;
+
+    for (const [original, regex] of Object.entries(substitutions)) {
+      pattern = pattern.replace(original, `[${original}${regex.source.slice(1, -1)}]*`);
+    }
+
+    const regex = new RegExp(`${pattern}\\w*`, 'gi');
     filteredText = filteredText.replace(regex, (match) => '*'.repeat(match.length));
   });
 
