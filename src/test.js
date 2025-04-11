34acec285608c67a5ba1b826157b96021e6571f0
GManTheMarioGuy <155935644+GManTheMarioGuy@users.noreply.github.com>
2025-04-10 19:32:29 -0500

    Update test.js

--- src/test.js
+++ src/test.js
@@ -1,12 +1,13 @@
-const { loadProfanity, add, filterText } = require('no-cursing');
+const { loadWords, add, filterText } = require('no-cursing');
 
 (async () => {
   try {
-    await loadProfanity();
-    add('123', 'SEVERITY:PROFANE');
-    add('1234', 'SEVERITY:SEXUAL');
-    add('12345', 'SEVERITY:OFFENSIVE');
-    const inputText = "This is a test with 123, 1234, and 12345 in it.";
+    await loadWords();
+    add('123', 'PROFANE');
+    add('1234', 'SEXUAL');
+    add('12345', 'OFFENSIVE');
+    add('123456', 'SAFE');
+    const inputText = "This is a test with 123, 1234, 12345, and 123456 in it.";
     const sanitizedText = filterText(inputText);
     console.log(sanitizedText);
   } catch (error) {
